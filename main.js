c4f6a7442fedc571b12484cacaea540c29f1191a
Dana Tran <dana.l.tran@gmail.com>
2016-09-23 07:52:28 -0700

    going through react lifecycle

---
+++ main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+ReactDOM.render(
+	<App />, document.getElementById('app')
+	);
+
+//bottom is only uses instead of above if the ReactDOM.render is used on App.js directly
+// import App from './App';
